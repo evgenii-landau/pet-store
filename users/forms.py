a1be427da98f8624f76defa388a4474de05f9b88
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-06-18 19:19:00 +0300

    created ProfileForm and profile view

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    UserChangeForm,
+)
 from django.contrib.auth import get_user_model
 
 
@@ -76,3 +79,25 @@ class UserRegisterForm(UserCreationForm):
             "password1",
             "password2",
         ]
+
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control py-4"})
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control py-4"})
+    )
+    image = forms.ImageField(
+        widget=forms.FileInput(attrs={"class": "custom-file-label"}), required=False
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control py-4", "readonly": True})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={"class": "form-control py-4", "readonly": True})
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ["first_name", "last_name", "image", "username", "email"]
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginUser, RegisterUser
+from .views import LoginUser, RegisterUser, profile
 
 app_name = "users"
 
@@ -7,4 +7,5 @@ app_name = "users"
 urlpatterns = [
     path("login/", LoginUser.as_view(), name="login"),
     path("register/", RegisterUser.as_view(), name="register"),
+    path("profile/", profile, name="profile"),
 ]
