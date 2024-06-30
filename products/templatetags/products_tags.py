cbab93d061c02a65c3bad55f8e535795fc22e991
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-06-30 10:52:41 +0300

    update profile section

--- products/templatetags/products_tags.py
+++ products/templatetags/products_tags.py
@@ -5,7 +5,12 @@ from static.vendor.data.footer import data
 register = template.Library()
 
 
-@register.inclusion_tag("products/footer.html")
+@register.inclusion_tag("products/inclusion/footer.html")
 def show_footer():
     footer_data = data
     return {"footer_data": footer_data}
+
+
+@register.inclusion_tag("products/inclusion/copyright.html")
+def show_copyright():
+    return {}
